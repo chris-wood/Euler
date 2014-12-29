c0176110833b8daada6f0697a01feb17557a6570
Christopher Wood <christopherwood07@gmail.com>
2014-12-29 15:19:00 -0800

    Fibpow

--- problems/001/sol.py
+++ problems/001/sol.py
@@ -1,19 +1,22 @@
 # Problem 1. Find the sum of all the multiples of 3 or 5 below 1000.
 
+import sys
+upperBound = int(sys.argv[1])
+
 def sumDivisibleByN(limit, n):
 	m = limit / n 
 	# sum_{i=0}^{m} = [m(m+1)] / 2
 	return n * ((m * (m + 1)) / 2)
 
 # fast way
-by3 = sumDivisibleByN(999, 3)
-by5 = sumDivisibleByN(999, 5)
-by15 = sumDivisibleByN(999, 15)
+by3 = sumDivisibleByN(upperBound - 1, 3)
+by5 = sumDivisibleByN(upperBound - 1, 5)
+by15 = sumDivisibleByN(upperBound - 1, 15)
 print (by3 + by5 - by15)
 
 # slow way
 ssum = 0
-for i in range(1000):
+for i in range(upperBound):
 	if (i % 3 == 0) or (i % 5 == 0):
 		ssum = ssum + i
 print ssum
