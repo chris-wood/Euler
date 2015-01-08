5cb77b3a1f8c56610fb9f09e7265dc0f1e4d6e0f
Christopher Wood <christopherwood07@gmail.com>
2015-01-08 09:59:38 -0800

    Created my own class to relearn OO in ruby

--- problems/005/sol.rb
+++ problems/005/sol.rb
@@ -4,33 +4,58 @@
 #
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-def sieve_product(n)
-	product = 1
-	marked = []
-	i = 1
-	while i < n # 2..10
-		marked.insert(i, 0)
-		i = i + 1
+require 'Prime'
+
+class PrimeFactor
+	def initialize(n, count)
+		@n = n 
+		@count = count
 	end
 
-	i = 2
-	while i < n
-		j = i
-		while j < n 
-			print j.to_s + " "
-			if marked[j - 1] == 0
-				marked[j - 1] = i # flag as the mark
+	def factor
+		@n
+	end
+
+	def count
+		@count
+	end
+end
+
+def prime_factors(n)
+	factors = Prime.prime_division(n)
+	factorList = []
+	factors.each {|f, c|
+		factorList << PrimeFactor.new(f, c)
+	}
+	return factorList
+end
+
+def find_minimum(x)
+	factorCounts = []
+
+	# initialize all factor counts to 0
+	for i in 2..x
+		factorCounts << 0
+	end
+
+	# build up the minimum required factors for even divisibility
+	x.downto(2) { |i| 
+		factors = prime_factors(i)
+		factors.each {|f| 
+			if (factorCounts[f.factor - 2] < f.count)
+				factorCounts[f.factor - 2] = f.count # bump up to the minimum number of factors
 			end
-			j = j + i
-		end
-		i = i + 1
-		while marked[i - 1] != 0
-			i = i + 1
-		end
+		}
+	}
+
+	# use the factor counts to actually compute the product
+	product = 1
+	for i in 2..x
+		product = product * (i ** factorCounts[i - 2])
 	end
 
 	return product
 end
 
-product = sieve_product(ARGV[0].to_i)
+product = find_minimum(ARGV[0].to_i)
 puts product
